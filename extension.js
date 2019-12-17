f5be70b55d0546cd3bc813ab137680aa9b710abc
PRATAP <pratap@fastmail.fm>
2019-12-17 23:48:23 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,10 @@
+function enable() { imports.ui.main.panel.statusArea.aggregateMenu._system._altSwitcher.actor.destroy(); }
 
-const Main = imports.ui.main;
+/* when you turnoff this extension, "gnome-shell" restart is required to bring the PowerOff Action button back. */
 
-function enable() { Main.panel.statusArea.aggregateMenu._system._altSwitcher.actor.destroy(); }
+/* This below function is a workaround to restart the gnome-shell but no proper documentation. So I am in Doubt and thus commented out. */
 
-function disable() { Main.panel.statusArea.aggregateMenu._system._altSwitcher.actor.show(); }
+/* function disable() { global.reexec_self(); } */
+
+function disable() { imports.misc.util.spawnCommandLine("zenity --no-wrap --info --text='Plz restart gnome-shell to bring back Poweroff Action Button by [Alt+F2 r Enter] method.'"); }
 
